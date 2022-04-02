81769cf955dd71ca8a4ea7c831f9e571bfb9ba4c
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-02 08:48:52 +0800

    add material scraping total pages

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,9 +12,42 @@ params = {
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'}
 
 result = requests.get(url, params=params, headers=headers)
-print(result.status_code)
+# print(result.status_code)
 
 soup = BeautifulSoup(result.text, 'html.parser')
 
-print(soup.prettify())
+def get_total_pages():
+    params = {
+        'q': 'Python Developer',
+        'l': 'New York State',
+        'vjk': '5f2fbaaa634be72b'
+    }
 
+    res = requests.get(url, params=params, headers=headers)
+
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+
+    with open('temp/res.html', 'w+') as outfile:
+        outfile.write(res.text)
+        outfile.close()
+
+    total_pages = []
+    # Scraping Step
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # print(soup.prettify())
+    pagination = soup.find('ul', 'pagination-list')
+    pages = pagination.findAll('li')
+    for page in pages:
+        # print(page.text)
+        total_pages.append(page.text)
+    # print(total_pages)
+
+    total = int(max(total_pages))
+    # print(total)
+    return total
+
+if __name__ == '__main__':
+    print(get_total_pages())

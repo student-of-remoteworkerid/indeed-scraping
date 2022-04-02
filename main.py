2fe701b1a66ca31c86c001c31578877ca138dc25
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-02 09:31:46 +0800

    add material scraping job item

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.indeed.com/jobs?'
+site = 'https://www.indeed.com'
+
 params = {
     'q': 'Python Developer',
     'l': 'New York State',
@@ -39,7 +41,7 @@ def get_total_pages():
     soup = BeautifulSoup(res.text, 'html.parser')
     # print(soup.prettify())
     pagination = soup.find('ul', 'pagination-list')
-    pages = pagination.findAll('li')
+    pages = pagination.find_all('li')
     for page in pages:
         # print(page.text)
         total_pages.append(page.text)
@@ -49,5 +51,42 @@ def get_total_pages():
     # print(total)
     return total
 
+def get_all_items():
+    params = {
+        'q': 'Python Developer',
+        'l': 'New York State',
+        'vjk': '5f2fbaaa634be72b'
+    }
+
+    res = requests.get(url, params=params, headers=headers)
+
+    with open('temp/res.html', 'w+') as outfile:
+        outfile.write(res.text)
+        outfile.close()
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # Scraping process
+    contents = soup.find_all('table', 'jobCard_mainContent big6_visualChanges')
+    # print(contents.)
+
+    # pick item
+    # * title
+    # * company name
+    # * company link
+    # * company address
+
+    for item in contents:
+        title = item.find('h2', 'jobTitle').text
+        company = item.find('span', 'companyName')
+        company_name = company.text
+        try:
+            company_link = site + company.find('a')['href']
+        except:
+            company_link = 'Link is not available'
+
+        print(company_link)
+
+
 if __name__ == '__main__':
-    print(get_total_pages())
+    # print(get_total_pages())
+    get_all_items()
